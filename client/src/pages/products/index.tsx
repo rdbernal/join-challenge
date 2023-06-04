2ad86bdfdb58f7e8ca1acc851d550f10935b7977
Rodrigo Bernal <rodrigobernal19@gmail.com>
2023-06-04 13:38:31 -0300

    style: add style to products page
    Add style to products page

--- client/src/pages/products/index.tsx
+++ client/src/pages/products/index.tsx
@@ -1,17 +1,30 @@
 // Components
+import Grid from '@mui/material/Grid'
 import BoxTitle from '../../layouts/components/boxTitle'
 import ProductCard from '../../layouts/components/productCard'
 
 const ProductsPage = () => {
   return (
-    <>
-      <BoxTitle title='Produtos'/>
+    <Grid container spacing={8}>
+      <Grid item xs={12}>
+        <BoxTitle title='Produtos' />
+      </Grid>
 
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-    </>
+      <Grid container item spacing={6}>
+        <Grid item lg={4} md={6} xs={12}>
+          <ProductCard />
+        </Grid>
+        <Grid item lg={4} md={6} xs={12}>
+          <ProductCard />
+        </Grid>
+        <Grid item lg={4} md={6} xs={12}>
+          <ProductCard />
+        </Grid>
+        <Grid item lg={4} md={6} xs={12}>
+          <ProductCard />
+        </Grid>
+      </Grid>
+    </Grid>
   )
 }
 
